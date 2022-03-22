6175276a4624aafd74ef0d9bd41282face057426
fabiano <fabianoo@gmail.com>
2022-03-22 17:26:27 -0300

    new exercise

---
+++ 2/6/Code.cpp
@@ -0,0 +1,46 @@
+K16+4=,-Hd;<,17V0f
+K16+4=,-Hd;<,41*V0f
+K16+4=,-Hd*1<;W;<,+SSV0f
+
+>71,Hz-57>-u)17:-;P16<Ht#%TH16<HN6TH16<H3QHC
+	WWH+7,->
+	16<H1eXcH
+	?014-HP1d6QHC
+		1.HPt#1%f3QHC
+			t#1%HeHt#6UY%cH
+			6HeH6UYc
+		EH-4;-HC
+			1HeH1SYcH
+		E
+	E
+	WWH+7,->
+E
+
+>71,Hq6;-:-P16<Ht#%TH16<HN6TH16<H-QHC
+	t#6%HeH-cH6HeH6SYc
+E
+
+>71,Hm;+:->-P16<Ht#%TH16<HN6QHC
+	;<,bb;7:<PtTtS6Qc
+	8:16<.PJ#JQcH.7:HP16<H1eXcH1d6cH1SSQH8:16<.PJM,JTHt#1%QcH8:16<.PJ%$6JQc
+E
+
+16<H5)16PQHC
+	16<H5)@'6HeHYXXXXXXc
+	16<HRHtHeHP16<HRQH5)447+P;1B-7.P16<QR5)@'6Qc
+	16<H6HeHXc
+	z-57>-u)17:-;PtT6T[QcHm;+:->-PtT6Qc
+	q6;-:-PtT6TYXQc
+	z-57>-u)17:-;PtT6T[QcHm;+:->-PtT6Qc
+	.7:HP16<H1e_cH1feYcH1UUQHC
+		q6;-:-PtT6T1Qc
+	E
+	z-57>-u)17:-;PtT6T[QcHm;+:->-PtT6Qc
+	z-57>-u)17:-;PtT6TXQcHm;+:->-PtT6Qc
+	.7:HP16<H1eXcH1d5)@'6cH1SSQHC
+		q6;-:-PtT6TP1MZeeXHgHZHbH[QQc
+	E
+	z-57>-u)17:-;PtT6TZQcHm;+:->-PtT6Qc
+	.:--PtQc
+	:-<=:6HXc
+E
