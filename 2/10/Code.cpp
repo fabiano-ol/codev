74687fb2582fdb778321fbff4f312ba8c4460781
fabiano <fabianoo@gmail.com>
2022-03-23 22:38:21 -0300

    improvement

---
+++ 2/10/Code.cpp
@@ -0,0 +1,59 @@
+K16+4=,-Hd;<,17V0f
+K16+4=,-Hd;<,41*V0f
+
+<A8-,-.H;<:=+<Hv7HC
+	16<Hmc
+	v7HRHx:7@c
+EHv7c
+
+>71,Hq6;-:-Pv7HRHNtTH16<H-QHC
+	v7HRH67>7HeHPv7HRQH5)447+P;1B-7.Pv7QQc
+	67>7UfmHeH-cH67>7Ufx:7@HeHtcHtHeH67>7cH
+E
+
+v7HRHq6>-:<-Pv7HRHtQHC
+	WWH+7,->
+	v7HRH8cHv7HRHt:HeHv}ttc
+	8HeHtc
+	?014-HP8HIeHv}ttQHC
+		q6;-:-Pt:TH8UfmQc
+		8HeH8Ufx:7@c
+	E
+	:-<=:6Ht:c
+	WWH+7,->
+E
+
+>71,Hm;+:->-Pv7HRHtQHC
+	v7HRH8HeHtc
+	8:16<.PJ#HJQcH.7:HP8etcH8Iev}ttcH8e8Ufx:7@QH8:16<.PJM,HJTH8UfmQcH8:16<.PJ%$6JQc
+E
+
+>71,Hl-;<:71Pv7HRHNtQHC
+	v7HRH8THRH8:cH8HeHtcH
+	?014-HP8HIeHv}ttQHC
+		8:HeH8cH8HeH8Ufx:7@cH.:--P8:Qc
+	E
+	tHeHv}ttcH
+E
+
+
+16<H5)16PQHC
+	16<H6cHv7HRHtHeHv}ttcHv7HRHt:c
+	?014-HP;+)6.PJM,JTHN6QfXQHC
+		1.HP6IeUYQHC
+			WRH4êH6TH-YTH-ZTHVVVTH-6HRW
+			.7:HP16<H1eXcH1d6cH1SSQHC
+				16<H-cH;+)6.PJM,JTHN-Qc	q6;-:-PtT-QcH
+			E
+			t:HeHq6>-:<-PtQcHm;+:->-Pt:QcHl-;<:71Pt:QcHl-;<:71PtQc
+		EH-4;-HC
+			WRH)=<75)<1+H<-;<HRW
+			6HeHYXXXXXXc
+			.7:HP16<H1eXcH1d6cH1SSQHC
+				q6;-:-PtT1MYXXQc
+			E
+			t:HeHq6>-:<-PtQcHm;+:->-Pt:QcHl-;<:71Pt:QcHl-;<:71PtQc
+		E
+	E
+	:-<=:6HXc
+E
