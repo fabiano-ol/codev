6b208124abeedc34adbb208594ea7635c2cd1dbc
gramado <fabiano.oliveira@ime.uerj.br>
2025-09-23 12:03:28 -0300

    ex 2/20

---
+++ 2/20/Code.cpp
@@ -0,0 +1,80 @@
+K16+4=,-Hd;<,17V0f
+K16+4=,-Hd;<,41*V0f
+K16+4=,-Hd*1<;W;<,+SSV0f
+
+<A8-,-.H;<:=+<Ht1;<)t16-):HC
+	16<HRHmc
+	16<Hvc
+EHt1;<)t16-):c
+
+t1;<)t16-):Hq6<-:;-+)7Pt1;<)t16-):HtYTHt1;<)t16-):HtZQHC
+	WWH+7,->
+	t1;<)t16-):Htc
+	tVmHeHP16<HRQH5)447+P;1B-7.P16<QHRHtYVvQcHWW6ú5-:7H5á@157H,-H-4-5-6<7;H6)H16<-:;-çã7
+	tVvHeHXc
+	.7:HP16<H1eXcH1dtYVvcH1SSQHC
+		.7:HP16<H2eXcH2dtZVvcH2SSQHC
+			1.HPtZVm#2%HeeHtYVm#1%QHC
+				tVm#tVv%HeHtYVm#1%c
+				tVvHeHtVvSYc
+				*:-)3c
+			E
+		E
+	E
+	:-<=:6Htc
+	WWH+7,->
+E
+
+>71,Hq6;-:-Pt1;<)t16-):HNtTH16<H-QHC
+	tVm#tVv%HeH-cHtVvHeHtVvSYc
+E
+
+>71,Hm;+:->-Pt1;<)t16-):HtQHC
+	;<,bb;7:<PtVmTtVmStVvQc
+	8:16<.PJ#JQcH.7:HP16<H1eXcH1dtVvcH1SSQH8:16<.PJM,HJTHtVm#1%QcH8:16<.PJ%$6JQc
+E
+
+t1;<)t16-):Hk'q6<-:;-+)7Pt1;<)t16-):HtYTHt1;<)t16-):HtZTH16<H9eYQHC
+K1.6,-.Hkwlm~
+	8:16<.PJkwlm~'jmoqv'm mk$6JQc
+K-6,1.
+	t1;<)t16-):H:c
+	WW.7:HP16<H<eXcH<d9cH<SSQHC
+	:HeHq6<-:;-+)7PtYTtZQc
+	WWE
+K1.6,-.Hkwlm~
+	8:16<.PJkwlm~'mvl'm mk$6JQc
+K-6,1.
+	:-<=:6H:c
+E
+
+16<H5)16PQHC
+	;-<*=.P;<,7=<THv}ttQcH;-<*=.P;<,-::THv}ttQc
+	16<H5)@'6YTH5)@'6ZcHt1;<)t16-):HtYcHt1;<)t16-):HtZcHt1;<)t16-):Htc
+	?014-HP;+)6.PJM,HM,JTHN5)@'6YTHN5)@'6ZQfXQHC
+		1.HP5)@'6YIeUYQHC
+			WRH4êH6YTH-YTH-ZTHVVVTH-6YTH6ZTHH-YTH-ZTHVVVTH-6ZHRW
+			tYVmHeHP16<HRQH5)447+P;1B-7.P16<QR5)@'6YQcHtYVvHeHXc
+			.7:HP16<H1eXcH1d5)@'6YcH1SSQHC
+				16<H-cH;+)6.PJM,JTHN-Qc	q6;-:-PtYT-Qc
+			E
+			tZVmHeHP16<HRQH5)447+P;1B-7.P16<QR5)@'6ZQcHtZVvHeHXc
+			.7:HP16<H1eXcH1d5)@'6ZcH1SSQHC
+				16<H-cH;+)6.PJM,JTHN-Qc	q6;-:-PtZT-Qc
+			E
+			tHeHk'q6<-:;-+)7PtYTtZQcHm;+:->-PtQc
+			.:--PtYVmQcH.:--PtZVmQcH.:--PtVmQc
+		EH-4;-HC
+			WRH)=<75)<1+H<-;<HRW
+			5)@'6YHeHaXXXcH5)@'6ZHeH5)@'6Yc
+			tYVmHeHP16<HRQH5)447+P;1B-7.P16<QR5)@'6YQcHtYVvHeHXc
+			tZVmHeHP16<HRQH5)447+P;1B-7.P16<QR5)@'6ZQcHtZVvHeHXc
+			.7:HP16<H1eXcH1d5)@'6YcH1SSQHC
+				q6;-:-PtYT1RZQcHq6;-:-PtZT1R[Qc
+			E
+			tHeHk'q6<-:;-+)7PtYTtZQcHm;+:->-PtQc
+			.:--PtYVmQcH.:--PtZVmQcH.:--PtVmQc
+		E
+	E
+	:-<=:6HXc
+E
